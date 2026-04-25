014f409eab821e50e40af88edd60735dd64d8de7
643gamedev (formally,Saral) <119824719+643-gamedev@users.noreply.github.com>
2026-04-25 06:49:04 -0600

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        neon: '#39ff14',
-        neonSoft: '#00ff9f',
-        dark: '#000000',
+        neon: "#39ff14",
+        neonSoft: "#00ff9f",
+        dark: "#000000",
       },
       fontFamily: {
-        mono: ['Cutive Mono', 'monospace'],
+        mono: ["Cutive Mono", "monospace"],
       },
     },
   },
   plugins: [],
 };
-
